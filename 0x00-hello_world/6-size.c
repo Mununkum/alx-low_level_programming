44353ddcc1bedb5fc784143aa62561abf07b3389
Mununkum <123147686+Mununkum@users.noreply.github.com>
2023-02-20 14:43:58 -0800

    betty is great

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,17 +5,17 @@
  */
 int main(void)
 {
-	int a;
-	char b;
+	char a;
+	int b;
 	long int c;
 	long long int d;
 	float f;
 
-	printf("size of int: %lu byte(s)\n", sizeof(a));
-	printf("size of char: %lu byte(s)\n", sizeof(b));
-	printf("size of long int: %lu byte(s)\n", sizeof(c));
-	printf("size of long long int: %lu byte(s)\n", sizeof(d));
-	printf("size of float: %lu byte(s)\n", sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
 
 }
