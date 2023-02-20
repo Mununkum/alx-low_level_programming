0dbd96aa1569a5ad698aa0ea580142f4f677d82e
Mununkum <123147686+Mununkum@users.noreply.github.com>
2023-02-20 13:36:06 -0800

    betty is good

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is a program for puzzle solving
+ *          and should be appreciated by all
+ * Return: 0 success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
