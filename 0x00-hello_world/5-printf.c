c7e7be4a3ada64d645f986c88606305b881bbece
Mununkum <123147686+Mununkum@users.noreply.github.com>
2023-02-20 13:59:03 -0800

    there is nothing greater than alx

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program outcome as a piece of ar
+ * Return: 0 successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
